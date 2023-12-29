074cef7d27294586c5354645be1211d47a9ad02d
Rajan <rajanmoliya2000@gmail.com>
2023-12-29 22:36:22 +0530

    intial commit

---
+++ src/components/Logo.js
@@ -0,0 +1,4 @@
+export default function Logo(){
+    return <h1>🌴️ Far Away 🎒️</h1>
+  }  
+  
---
+++ src/components/PackingList.js
@@ -0,0 +1,9 @@
+import Item from "./Item";
+
+export default function PackingList({ items, onDeleteItem, onToggleItem }) {
+  return (<div className="list">
+    <ul>
+      {items.map(item => <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id} />)}
+    </ul>
+  </div>);
+}
